1f0ef17dca336dfda4709c58ccc8a7ee1e9970f4
Brando Jesus <brando0jesus@gmail.com>
2022-10-15 23:59:11 -0500

    Creation of classes on JavaScrip and PHP in diferent files

---
+++ JS/Account.js
@@ -0,0 +1,7 @@
+function Account() {
+    this.id;        // this -> reserved word,  
+    this.name;      //.(point) -> use point from an instanced object
+    this.document;
+    this.email;
+    this.password;
+}
---
+++ PHP/Route.php
@@ -0,0 +1,6 @@
+<?php
+class Route {
+    public $id = integer;
+    public $init = Array();     // Array() == []
+    public $end = [];
+}
