d1186861aa0332f454fdf59991cfaaf30240d148
Brando Jesus <brando0jesus@gmail.com>
2022-10-17 21:29:07 -0500

    Constructor methods declaration for 4 laguajes

--- JS/Account.js
+++ JS/Account.js
@@ -1,7 +1,23 @@
-function Account() {
+/*
+function Account(name, document) {
     this.id;        // this -> reserved word,  
-    this.name;      //.(point) -> use point from an instanced object
-    this.document;
+    this.name = name;      //.(point) -> use point from an instanced object
+    this.document = document;
     this.email;
     this.password;
+}
+*/
+// TODO we just could put the mouse over the 3 points of function and select Quick fix to apply EcmaScript 6
+
+// USING  EcmaScript 6 STANDAR 
+class Account {
+
+    constructor(name, document) {
+        this.id;
+        this.name = name;
+        this.document = document;
+        this.email;
+        this.password;
+    }
+
 }
--- PYTHON/account.py
+++ PYTHON/account.py
@@ -4,3 +4,7 @@ class Account:      # we have to use Upper comand case
     document    = str
     email       = str
     password    = str
+
+    def __init__(self, name, document):
+        self.name       = name
+        self.document   = document
